f4f0c9ca349f46b17131d9383216d7ab2b9b71bf
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-15 16:40:14 +0100

    Initilizing Patient service.
    Firstly, We creating the class
    And add 'getAllPatients' method

--- src/main/java/com/healthcaredental/reception/Patient/PatientService.java
+++ src/main/java/com/healthcaredental/reception/Patient/PatientService.java
@@ -1,2 +1,26 @@
-package com.healthcaredental.reception.Patient;public class PatientService {
+package com.healthcaredental.reception.Patient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+
+    public List<Patient> getAllPatients(){
+
+        List<Patient> patients= new ArrayList<Patient>();
+
+        patientRepository.findAll()
+                .forEach(patients::add);
+
+        return patients;
+
+    }
 }
