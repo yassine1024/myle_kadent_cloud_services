7df84f999e5de1e7cc9e0946a13e1f879c8e899d
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-15 16:24:16 +0100

    Adding PatientRepository

---
+++ src/main/java/com/healthcaredental/reception/Patient/PatientRepository.java
@@ -0,0 +1,6 @@
+package com.healthcaredental.reception.Patient;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientRepository extends CrudRepository<Patient, String> {
+}
