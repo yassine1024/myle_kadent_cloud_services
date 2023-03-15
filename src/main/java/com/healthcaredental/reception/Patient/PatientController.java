13852d305e024f6bc046e29a771ed52daa76a558
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-15 16:52:26 +0100

    Initializing Patient controller.
    Firstly, making the '/api/patients' as a root route of the controller
    Secondly, Adding 'getAllPatients' in the GET method of root's route

--- src/main/java/com/healthcaredental/reception/Patient/PatientController.java
+++ src/main/java/com/healthcaredental/reception/Patient/PatientController.java
@@ -1,2 +1,24 @@
-package com.healthcaredental.reception.Patient;public class PatientController {
+package com.healthcaredental.reception.Patient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+    @GetMapping("")
+    public List<Patient> getAllPatients(){
+
+            return patientService.getAllPatients();
+    }
+
+
+
 }
