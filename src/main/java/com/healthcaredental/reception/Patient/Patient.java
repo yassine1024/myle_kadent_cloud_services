22e89ddf356fa19daa13e9ceb69c20264c779c73
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-15 16:16:29 +0100

    Add Patient Entity.
    This class means that we manipulte patients table in the databse as Object(ORM).
    Its attributes are only the core. It can be more.

---
+++ src/main/java/com/healthcaredental/reception/Patient/Patient.java
@@ -0,0 +1,63 @@
+package com.healthcaredental.reception.Patient;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Patient {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+
+    public Patient(String id, String firstName, String lastName, String phoneNumber, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ src/main/java/com/healthcaredental/reception/Patient/PatientController.java
@@ -0,0 +1,2 @@
+package com.healthcaredental.reception.Patient;public class PatientController {
+}
