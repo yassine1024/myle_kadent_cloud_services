4c087e164d99c683cbbd001848dd04b244574792
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-17 19:01:53 +0100

    Create a test unit that tests PatientService.
    Implementing the method that tests adding a new Patient and returning allPatients in the same method,
    TestConfig is using to use embeded database as database for testing (derby).

--- src/test/java/com/healthcaredental/reception/patient/PatientServiceTests.java
+++ src/test/java/com/healthcaredental/reception/patient/PatientServiceTests.java
@@ -1,2 +1,46 @@
-package com.healthcaredental.reception.patient;public class PatientServiceTests {
+package com.healthcaredental.reception.patient;
+
+
+import com.healthcaredental.reception.Patient.Patient;
+import com.healthcaredental.reception.Patient.PatientService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(TestConfig.class)
+public class PatientServiceTests {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Test
+    public void testAddPatient() {
+        for (int i = 0; i < 11; i++) {
+
+
+            Patient patient = new Patient();
+            patient.setId("" + i);
+            patient.setFirstName("Atik" + i);
+            patient.setLastName("Yassine" + i);
+            patient.setPhoneNumber("05456232" + i);
+            patient.setAddress("Blida");
+            patientService.addPatient(patient);
+
+
+        }
+
+        patientService.getAllPatients().forEach(patient1 -> {
+            System.out.println("Patient: " + patient1.getFirstName() + " " + patient1.getLastName() + " ID: " + patient1.getId());
+        });
+
+    }
+
+
 }
--- src/test/java/com/healthcaredental/reception/patient/TestConfig.java
+++ src/test/java/com/healthcaredental/reception/patient/TestConfig.java
@@ -1,2 +1,20 @@
-package com.healthcaredental.reception.patient;public class TestConfig {
+package com.healthcaredental.reception.patient;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+
+@TestConfiguration
+public class TestConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.DERBY).build();
+
+    }
 }
