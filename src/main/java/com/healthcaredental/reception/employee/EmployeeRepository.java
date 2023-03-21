9929cc2fe5f24b09b52abface8208b3846a32507
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-21 20:16:16 +0100

    Add EmployeeRepository and declare findByCabinetId method on it.

--- src/main/java/com/healthcaredental/reception/employee/EmployeeRepository.java
+++ src/main/java/com/healthcaredental/reception/employee/EmployeeRepository.java
@@ -1,2 +1,10 @@
-package com.healthcaredental.reception.employee;public interface EmployeeRepository {
+package com.healthcaredental.reception.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+    public List<Employee> findByCabinetId(String id);
 }
