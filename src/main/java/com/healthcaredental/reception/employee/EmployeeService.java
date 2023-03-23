4031acedc483438c035e8a29170390b1dcda1183
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2023-03-23 06:29:15 +0100

    Add EmployeeService and implement all CRUD methodes.

--- src/main/java/com/healthcaredental/reception/employee/EmployeeService.java
+++ src/main/java/com/healthcaredental/reception/employee/EmployeeService.java
@@ -1,2 +1,42 @@
-package com.healthcaredental.reception.employee;public class EmployeeService {
+package com.healthcaredental.reception.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees(String cabinetId) {
+
+        List<Employee> employees = new ArrayList<>();
+
+        employeeRepository.findByCabinetId(cabinetId).addAll(employees);
+               // .forEach(employees::add);
+        return employees;
+
+    }
+
+    public Employee getEmployee(String id) {
+
+        return employeeRepository.findById(id).get();
+    }
+
+    public void addEmployee(Employee employee) {
+
+        employeeRepository.save(employee);
+    }
+
+    public void updateEmployee(Employee employee) {
+
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(String id) {
+
+        employeeRepository.deleteById(id);
+    }
 }
