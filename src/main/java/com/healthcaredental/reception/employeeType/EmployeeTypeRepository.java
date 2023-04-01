b1288862657c7d49fe704cc536a172367958ee68
yassine1024 <atik.yassine.1024@gmail.com>
2023-04-01 16:35:51 +0200

    Delete all employeeType package and its classes.

--- src/main/java/com/healthcaredental/reception/employeeType/EmployeeTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.healthcaredental.reception.employeeType;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeTypeRepository extends JpaRepository<EmployeeType, Integer> {
-}
