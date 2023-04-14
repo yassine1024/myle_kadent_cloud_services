3d1f38d56c28f3b75e8a9f4040d8f211582613a5
yassine1024 <atik.yassine.1024@gmail.com>
2023-04-14 17:54:52 +0200

    Add Queue entity that tracked today's rendezvous.

---
+++ src/main/java/com/healthcaredental/reception/Queue/Queue.java
@@ -0,0 +1,29 @@
+package com.healthcaredental.reception.Queue;
+
+import com.healthcaredental.reception.rendezvous.Rendezvous;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+@Entity
+@Data
+public class Queue {
+
+    @Id
+    private Long rendezvousId;
+
+    private  boolean isArrive;
+    // time with this format HH:MM:SS
+    private  String arriveTime;
+    //Inside means that the patient is taking medical care
+    private boolean isInside;
+    private boolean isOutside;
+    private String quitTime;
+
+    @OneToOne
+    @MapsId
+    private Rendezvous rendezvous;
+}
