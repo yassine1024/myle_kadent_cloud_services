1ab111c8ea83e10daf8baee04092b37ba5eeb7dd
YASSINE ATIK <atik.yassine.1024@gmail.com>
2025-02-03 09:38:33 +0100

    Add JsonProperty annotation to fix the desrialization of queue(specially for the boolean attributes)

--- src/main/java/com/healthcaredental/reception/Queue/Queue.java
+++ src/main/java/com/healthcaredental/reception/Queue/Queue.java
@@ -1,5 +1,6 @@
 package com.healthcaredental.reception.Queue;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.healthcaredental.reception.rendezvous.Rendezvous;
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
@@ -16,13 +17,15 @@ public class Queue {
 
     @Id
     private Long rendezvousId;
-
+    @JsonProperty("isArrive")
     private  boolean isArrive;
     // time with this format HH:MM:SS
     private  String arriveTime;
     //Inside means that the patient is taking medical care
+    @JsonProperty("isInside")
     private boolean isInside;
     private String insideTime;
+    @JsonProperty("isOutside")
     private boolean isOutside;
     private String quitTime;
 
