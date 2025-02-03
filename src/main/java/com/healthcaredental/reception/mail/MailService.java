8ac9f20692d19ecd8dd7cba3d11a1f0d4b717dff
YASSINE ATIK <atik.yassine.1024@gmail.com>
2025-02-03 15:46:06 +0100

    Prepare the core part for sending mails with MailService

---
+++ src/main/java/com/healthcaredental/reception/mail/MailService.java
@@ -0,0 +1,24 @@
+package com.healthcaredental.reception.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom("your-email@gmail.com");
+
+        mailSender.send(message);
+    }
+}
