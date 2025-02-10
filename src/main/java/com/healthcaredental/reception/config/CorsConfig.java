6ee16e19e8f7c5dc7893b31db685e9a6cace086d
YASSINE ATIK <atik.yassine.1024@gmail.com>
2025-02-10 11:58:48 +0100

    Fix Cors issue

--- src/main/java/com/healthcaredental/reception/config/CorsConfig.java
+++ src/main/java/com/healthcaredental/reception/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200") // Update with your Angular app's origin
+                .allowedOrigins("http://127.0.0.1", "http://localhost") // Update with your Angular app's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
